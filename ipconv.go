54e09f28fb83722ed116ed3b23dfeb8b12538a2e
PraserX <tpekar@volny.cz>
2018-05-15 16:22:23 +0200

    Initial commit

---
+++ ipconv.go
@@ -0,0 +1,86 @@
+// Package ipconv provides basic conversion between IP addresses
+// representations. Converter does not require additional settings.
+package ipconv
+
+import (
+	"encoding/binary"
+	"math/big"
+	"net"
+)
+
+// IPConverter definition.
+type IPConverter struct {
+}
+
+// New create new IP converter. There is additional configuration.
+func New() *IPConverter {
+	return &IPConverter{}
+}
+
+// IPv4ToInt converts IP address of version 4 from net.IP to uint32
+// representation.
+func (i *IPConverter) IPv4ToInt(ipaddr net.IP) uint32 {
+	return binary.BigEndian.Uint32(ipaddr.To4())
+}
+
+// IPv6ToInt converts IP address of version 6 from net.IP to uint64 array
+// representation. Return value contains high integer value on the first
+// place and low integer value on second place.
+func (i *IPConverter) IPv6ToInt(ipaddr net.IP) [2]uint64 {
+	// Get two separates values of integer IP
+	ip := [2]uint64{
+		binary.BigEndian.Uint64(ipaddr.To16()[0:8]),  // IP high
+		binary.BigEndian.Uint64(ipaddr.To16()[8:16]), // IP low
+	}
+
+	return ip
+}
+
+// IPv6ToBigInt converts IP address of version 6 from net.IP to math big
+// integer representation.
+func (i *IPConverter) IPv6ToBigInt(ipaddr net.IP) big.Int {
+	// Define big int variable
+	var ip big.Int
+
+	// Initialize value as bytes
+	ip.SetBytes(ipaddr)
+
+	return ip
+}
+
+// IntToIPv4 converts IP address of version 4 from integer to net.IP
+// representation.
+func (i *IPConverter) IntToIPv4(ipaddr uint32) net.IP {
+	// Allocate 4 bytes IP
+	ip := make(net.IP, 4)
+
+	// Proceed conversion
+	binary.BigEndian.PutUint32(ip, ipaddr)
+
+	return ip
+}
+
+// IntToIPv6 converts IP address of version 6 from integer (high and low value)
+// to net.IP representation.
+func (i *IPConverter) IntToIPv6(high, low uint64) net.IP {
+	// Allocate 16 bytes IP
+	ip := make(net.IP, 16)
+
+	// Allocate 8 bytes arrays for IPs
+	ipHigh := make([]byte, 8)
+	ipLow := make([]byte, 8)
+
+	// Proceed conversion
+	binary.BigEndian.PutUint64(ipHigh, high)
+	binary.BigEndian.PutUint64(ipLow, low)
+
+	for i := 0; i < 16; i++ {
+		if i < 8 {
+			ip[i] = ipHigh[i]
+		} else if i >= 8 {
+			ip[i] = ipLow[i-8]
+		}
+	}
+
+	return ip
+}
---
+++ ipconv_test.go
@@ -0,0 +1,86 @@
+package ipconv
+
+import (
+	"net"
+	"testing"
+)
+
+func TestIPv4ToInt(t *testing.T) {
+	for _, c := range []struct {
+		in   net.IP
+		want uint32
+	}{
+		{net.ParseIP("192.168.1.1"), 3232235777},
+		{net.ParseIP("0.0.0.0"), 0},
+		{net.ParseIP("8.8.8.8"), 134744072},
+		{net.ParseIP("255.255.255.255"), 4294967295},
+	} {
+		got := New().IPv4ToInt(c.in)
+		if got != c.want {
+			t.Errorf("IPv4ToInt(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func TestIPv6ToInt(t *testing.T) {
+	for _, c := range []struct {
+		in   net.IP
+		want [2]uint64
+	}{
+		{net.ParseIP("0000:0000:0000:0000:0000:0000:0000:0000"), [2]uint64{0, 0}},
+		{net.ParseIP("0000:0000:0000:0000:0000:0000:0000:1"), [2]uint64{0, 1}},
+		{net.ParseIP("2001:4860:4860::8888"), [2]uint64{2306204062558715904, 34952}},
+	} {
+		got := New().IPv6ToInt(c.in)
+		if got != c.want {
+			t.Errorf("IPv6ToInt(%q) == %q, want %q", c.in.To16(), got, c.want)
+		}
+	}
+}
+
+// func TestIPv6ToBigInt(t *testing.T) {
+// 	for _, c := range []struct {
+// 		in   net.IP
+// 		want *big.Int
+// 	}{
+// 		{net.ParseIP("2001:0:0:0:0:ffff:c0a8:101"), big.NewInt(42540488161975842760550637899214225665)},
+// 	} {
+// 		got := New().IPv6ToBigInt(c.in)
+// 		if got != c.want {
+// 			t.Errorf("IPv6ToInt(%q) == %q, want %q", c.in.To16(), got, c.want)
+// 		}
+// 	}
+// }
+
+func TestIntToIPv4(t *testing.T) {
+	for _, c := range []struct {
+		in   uint32
+		want net.IP
+	}{
+		{3232235777, net.ParseIP("192.168.1.1")},
+		{0, net.ParseIP("0.0.0.0")},
+		{134744072, net.ParseIP("8.8.8.8")},
+		{4294967295, net.ParseIP("255.255.255.255")},
+	} {
+		got := New().IntToIPv4(c.in)
+		if !got.Equal(c.want) {
+			t.Errorf("IntToIPv4(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func TestIntToIPv6(t *testing.T) {
+	for _, c := range []struct {
+		in   [2]uint64
+		want net.IP
+	}{
+		{[2]uint64{0, 0}, net.ParseIP("0000:0000:0000:0000:0000:0000:0000:0000")},
+		{[2]uint64{0, 1}, net.ParseIP("0000:0000:0000:0000:0000:0000:0000:1")},
+		{[2]uint64{2306204062558715904, 34952}, net.ParseIP("2001:4860:4860::8888")},
+	} {
+		got := New().IntToIPv6(c.in[0], c.in[1])
+		if !got.Equal(c.want) {
+			t.Errorf("IntToIPv6(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
